8acdf1bdd73deee65a39dd55e133cb0e2eea586c
Dhass <t.bhags21@gmail.com>
2015-03-28 18:49:15 -0500

    - Created a basic server and client using websockets

---
+++ server/src/client/WebClientEndpoint.java
@@ -0,0 +1,70 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Logger;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.DeploymentException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+
+import org.glassfish.tyrus.client.ClientManager;
+
+@ClientEndpoint
+public class WebClientEndpoint {
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+	private static CountDownLatch latch;
+
+	@OnOpen
+	public void onOpen(Session session) {
+		logger.info("Connected ... " + session.getId());
+		try {
+			session.getBasicRemote().sendText("start");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@OnMessage
+	public String onMessage(String message, Session session) {
+		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(
+				System.in));
+		try {
+			logger.info("Received ...." + message);
+			String userInput = bufferRead.readLine();
+			return userInput;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@OnClose
+	public void onClose(Session session, CloseReason closeReason) {
+		logger.info(String.format("Session %s close because of %s",
+				session.getId(), closeReason));
+	}
+
+	public static void main(String[] args) {
+		latch = new CountDownLatch(1);
+
+		ClientManager client = ClientManager.createClient();
+		try {
+			client.connectToServer(WebClientEndpoint.class, new URI(
+					"ws://localhost:8025/websockets/collabserver"));
+			latch.await();
+
+		} catch (DeploymentException | URISyntaxException
+				| InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
